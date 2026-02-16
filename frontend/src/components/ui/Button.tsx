6e59c70392ed7b93e628d73724ac243c6739ba94
aceraline <alix.ceraline@gmail.com>
2026-02-16 21:25:22 +0100

    16/02/2026: Page Home faite, Ajout d un style d ecriture

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps{
 export function Button({label, onClick, type = "button"}: ButtonProps)
 {
     return(
-        <button className="w-full bg-primary text-white font-bold py-3 rounded-xl hover:opacity-90" onClick={onClick} type={type}>
+        <button className="w-full bg-[#A69292]/86 text-black text-2xl font-bodini font-bold py-3 rounded-xl hover:opacity-90" onClick={onClick} type={type}>
             {label} 
             
         </button>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,23 @@
 import { Button } from "../components/ui/Button";
 import { useNavigate } from "react-router-dom";
+import { Heart } from "lucide-react";
 
 export function Home()
 {
     const navigate = useNavigate();
     return(
-        <div className="min-h-screen flex flex-col items-center justify-center p-4">
-            <h1 className="text-4xl font-bold mb-8"> Roocool </h1>
-            <div className="w-full max-w-sm space-y-4">
-            <Button label="Login" onClick={()=> navigate("auth/login")}/>
-            <Button label = "Sign up" onClick={()=> navigate("auth/register")}/>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#EBB3B8]/40 space-y-8 p-6">
+            <div className="text-center space-y-2 mt-16">
+            <h1 className="text-5xl font-bodini font-bold text-black mb-8"> Roocool </h1>
             </div>
+            <div className="mb-8 animate-bounce" >
+                <Heart className="w-48 h-48 text-primary  fill-current"/>
+            </div>         
+            <div className="w-full max-w-sm space-y-4 mt-12">
+                <Button label="Login" onClick={()=> navigate("auth/login")}/>
+                <Button label = "Sign up" onClick={()=> navigate("auth/register")}/>
+            </div>
+              <footer className="mt-auto pb-4 font-bodini text-xs text-gray-400">© 2026 Matcha Inc.</footer>
         </div>
     );
 }
